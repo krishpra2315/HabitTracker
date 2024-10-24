d578e84dd1095d4de8a8c0be7ef3392f96f1ea93
Krish Prasad <krishkp00@gmail.com>
2024-10-24 18:50:43 -0400

    Project Setup

---
+++ HabitTracker/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  HabitTracker
+//
+//  Created by Krish Prasad on 10/24/24.
+//
+
+import Foundation
+
+import SwiftUI
+
+@Observable
+class HabitList: Identifiable {
+    
+}
---
+++ HabitTracker/ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ViewModel.swift
+//  HabitTracker
+//
+//  Created by Krish Prasad on 10/24/24.
+//
+
+import Foundation
+
+import SwiftUI
+
+@Observable
+class ViewModel {
+   
+}
