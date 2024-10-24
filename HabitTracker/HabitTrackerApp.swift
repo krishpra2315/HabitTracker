eab739715e9f55f7bd0f69290f4ebef9a25ee7ba
Krish Prasad <krishkp00@gmail.com>
2024-10-24 18:38:15 -0400

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Krish Prasad on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
