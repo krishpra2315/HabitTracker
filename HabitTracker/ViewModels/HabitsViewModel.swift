d79f1af3981000fcccbf7f3a6bf280709838d9db
Krish Prasad <krishkp00@gmail.com>
2024-11-01 18:32:47 -0400

    fix log process method

--- HabitTracker/ViewModels/HabitsViewModel.swift
+++ HabitTracker/ViewModels/HabitsViewModel.swift
@@ -11,12 +11,16 @@ import SwiftUI
 
 class HabitsViewModel: ObservableObject {
     @Published var habitsList = [HabitsModel]()
+    @Published var completedGoals = [HabitsModel]()
     
     func addHabit() {
         habitsList.append(HabitsModel(name: "Habit1", label: "f", color: Color.red, goal: 10, unit: "hr", rep: Repeat.WEEKLY))
     }
     
-    func logProgress() {
-        habitsList[0].progress += 5
+    func logProgress(index: Int, amount: Int) {
+        habitsList[index].progress += amount
+        if (habitsList[index].progress >= habitsList[index].goal) {
+            completedGoals.append(habitsList[index])
+        }
     }
 }
