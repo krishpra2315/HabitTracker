3a47de0217b02590c345c240ce0cc2bd66615943
Aryan Patel <arypat2005@gmail.com>
2024-11-14 19:02:20 -0500

    new

--- HabitTracker/Views/HabitsView.swift
+++ HabitTracker/Views/HabitsView.swift
@@ -12,34 +12,38 @@ struct HabitsView: View {
     @State var showOverlay: Bool = false
     
     var body: some View {
-        VStack {
-            List(viewModel.habitsList, id: \.id, rowContent: { habit in
-                HStack {
-                    Text(habit.name)
-                    Text("\(habit.progress)")
-                    Text(habit.rep.rawValue)
+        NavigationView {
+            VStack {
+                List(viewModel.habitsList, id: \.id, rowContent: { habit in
+                    HStack {
+                        Text(habit.name)
+                        Text("\(habit.progress)")
+                        Text(habit.rep.rawValue)
+                    }
+                })
+                Button("add", action: {
+                })
+                .sheet(isPresented: $showOverlay) {
+                    AddHabitView(viewModel: viewModel)
                 }
-            })
-            Button("add", action: {
-                showOverlay.toggle()
-            })
-            .sheet(isPresented: $showOverlay) {
-                AddHabitView(viewModel: viewModel)
-            }
-            Button("edit", action: {
-                viewModel.logProgress(index: 0, amount: 5)
-            })
-            Button(action: {
-                print("Button was tapped.")
-            }){
-                Image(systemName: "plus")
-                    .resizable()
-                    .frame(width: 20, height:  20)
-                    .foregroundColor(.blue)
-
+                Button("edit", action: {
+//                    viewModel.logProgress(index: 0, amount: 5)
+                })
+                Button(action: {
+                    
+                }){
+                    Image(systemName: "plus")
+                        .resizable()
+                        .frame(width: 20, height:  20)
+                        .foregroundColor(.blue)
+                    
+                }
             }
         }
-        .padding()
+        .navigationTitle("Habits")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbarColorScheme(.light, for: .navigationBar)
+        .toolbarBackground(.visible, for: .navigationBar)
     }
 }
 
