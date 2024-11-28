48a991ba2679171fdbba9f44c229a9ce6c628cc9
seanyse <seanyan0915@gmail.com>
2024-11-28 18:37:32 -0500

    habit grouping and sort by alphabet

--- HabitTracker/ViewModels/HabitsViewModel.swift
+++ HabitTracker/ViewModels/HabitsViewModel.swift
@@ -12,9 +12,13 @@ import SwiftUI
 class HabitsViewModel: ObservableObject {
     @Published var habitsList = [HabitsModel]()
     @Published var completedGoals = [HabitsModel]()
+    var groupedHabits: [String: [HabitsModel]] {
+        Dictionary(grouping: habitsList) { $0.rep.rawValue }
+    }
     
     func addHabit(name: String, label: String, color: Color, goal: Int, unit: String, rep: Repeat) {
         habitsList.append(HabitsModel(name: name, label: label, color: color,  goal: goal, progress: 0, unit: unit, rep: rep))
+        habitsList.sort {$0.name < $1.name} // alphebetic sorting
     }
     
     func removeHabit(habit: HabitsModel) {
