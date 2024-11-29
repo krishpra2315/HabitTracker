6e87ee6a036fc06d8df3c18a82c6075c6187cf04
Aryan Patel <arypat2005@gmail.com>
2024-11-29 01:27:59 -0500

    Update to button positions

--- HabitTracker/Views/HabitsView.swift
+++ HabitTracker/Views/HabitsView.swift
@@ -80,24 +80,43 @@ struct HabitsRow: View {
     @State var showLogHabit: Bool = false
     
     var body: some View {
-        HStack {
-            VStack {
-                Text("\(habit.name)")
-                HStack {
-                    ZStack{
-                        Circle()
-                            .fill(habit.color)
-                            .frame(width: 47, height: 47)
-                        Text("\(habit.label)")
-                    }
-                    Text("\(habit.progress) /")
-                        .font(.largeTitle)
-                    VStack {
-                        Text("\(habit.goal)")
-                        Text("\(habit.unit)")
-                    }
-                }
+//        HStack {
+//        VStack {
+        HStack(spacing: 8) {
+            ZStack {
+                Circle()
+                    .fill(habit.color)
+                    .frame(width: 47, height: 47)
+                    .offset(x: -22)
+                
+                Text("\(habit.label)")
+                    .font(.system(size: 15))
+                    .offset(x: -22)
             }
+            .padding(.leading, 3)
+
+            ZStack {
+                
+                Text("0")
+                    .font(.system(size: 40))
+                    .offset(x: -21, y: 16)
+
+                Text(" /")
+                    .font(.system(size: 34))
+                    .offset(x: -5, y: 14)
+
+                Text("\(habit.goal)")
+                    .font(.system(size: 14))
+                    .offset(x: 15, y: 10)
+
+                Text("\(habit.unit)")
+                    .font(.system(size: 14))
+                    .offset(x: 18, y: 26)
+            }
+
+            Text("\(habit.name)")
+                .font(.system(size: 15))
+                .offset(x: -50, y: -20)
             Spacer()
             Button(action: {
                 showLogHabit.toggle()
@@ -105,7 +124,7 @@ struct HabitsRow: View {
                 ZStack{
                     Image(systemName: "capsule.fill")
                         .resizable()
-                        .frame(width: 60, height: 30)
+                        .frame(width: 70, height: 30)
                         .foregroundColor(.blue)
                     
                     Text("+ Log")
@@ -114,7 +133,7 @@ struct HabitsRow: View {
                 }
             }
         }
-        .padding(25)
+        .padding()
         .sheet(isPresented: $showLogHabit) { LogHabitView(habit: $habit, showLogHabit: $showLogHabit)
         }
     }
